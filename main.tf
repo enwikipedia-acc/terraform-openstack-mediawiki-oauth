fbc4fd68501cf4dd3190807ae00280a89bccd5ed
Simon Walker <github@stwalkerster.co.uk>
2023-01-12 00:42:16 +0000

    Upgrade dependencies

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     openstack = {
       source  = "terraform-provider-openstack/openstack"
-      version = "~> 1.49.0"
+      version = "~> 1.49"
     }
 
     cloudvps = {
